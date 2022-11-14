301504f14932aa52bcc445b440808e7d324cc0c9
proalekse1 <proaleekse1@yandex.ru>
2022-11-14 19:08:22 +0300

    first commit

---
+++ app/src/main/java/com/proalekse1/wetherapp7/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.proalekse1.wetherapp7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
