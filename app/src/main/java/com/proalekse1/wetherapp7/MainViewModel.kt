77d51a993ce791c0b45dec5985ce94a63bf01072
proalekse1 <proaleekse1@yandex.ru>
2022-11-17 17:57:17 +0300

    add MainViewModel

---
+++ app/src/main/java/com/proalekse1/wetherapp7/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.proalekse1.wetherapp7
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>() //для обновления большого фрагмента с погодой
+    val liveDataList = MutableLiveData<String>() //для обновления списка с погодой
+}
