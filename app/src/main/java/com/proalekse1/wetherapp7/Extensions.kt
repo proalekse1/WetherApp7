4569ab281b1e962a254907cb2dec89b1763197d1
proalekse1 <proaleekse1@yandex.ru>
2022-11-19 19:12:56 +0300

    add Extensions.kt and add permission fun

---
+++ app/src/main/java/com/proalekse1/wetherapp7/Extensions.kt
@@ -0,0 +1,13 @@
+package com.proalekse1.wetherapp7
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.isPermissionGranted(p: String): Boolean { //проверяет включен или нет интернет
+
+    return ContextCompat.checkSelfPermission( //возвращает 0 или -1 есть разрешение или нет разрешения
+        activity as AppCompatActivity, p) == PackageManager.PERMISSION_GRANTED
+
+}
--- app/src/main/java/com/proalekse1/wetherapp7/fragments/MainFragment.kt
+++ app/src/main/java/com/proalekse1/wetherapp7/fragments/MainFragment.kt
@@ -1,14 +1,21 @@
 package com.proalekse1.wetherapp7.fragments
 
+
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.proalekse1.wetherapp7.databinding.FragmentMainBinding
+import com.proalekse1.wetherapp7.isPermissionGranted
 
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String> //для диалога и разрешений
     private lateinit var binding: FragmentMainBinding//подключаем байндинг
 
     override fun onCreateView(
@@ -19,8 +26,26 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission() // диалог разрешений
+    }
 
+    private fun permissionListener(){ // диалог для разрешения
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show() //временный тостик чтоб узнать работает функция или нет
+        }
+    }
+
+    private fun checkPermission(){ //проверяем разрешение если нет то false с ! будет тру и код сработает
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
     }
