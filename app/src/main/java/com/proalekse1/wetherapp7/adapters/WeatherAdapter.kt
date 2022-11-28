8d0af5f75856f7ff501c6e88b923e62790d0b5b2
proalekse1 <proaleekse1@yandex.ru>
2022-11-28 16:49:47 +0300

    completed recycler view

--- app/src/main/java/com/proalekse1/wetherapp7/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/proalekse1/wetherapp7/adapters/WeatherAdapter.kt
@@ -1,11 +1,15 @@
 package com.proalekse1.wetherapp7.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.proalekse1.wetherapp7.R
 import com.proalekse1.wetherapp7.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
 
     class Holder(view: View) : RecyclerView.ViewHolder(view){ //хранит в себе разметк одного элемента
         val binding = ListItemBinding.bind(view)
@@ -14,5 +18,25 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>(){ //обновляет список
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem //сравнивает старый и новый элемент
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder { //элемент создается
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) { //элемент заполняется
+        holder.bind(getItem(position))
     }
 }
--- app/src/main/java/com/proalekse1/wetherapp7/fragments/HoursFragment.kt
+++ app/src/main/java/com/proalekse1/wetherapp7/fragments/HoursFragment.kt
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.proalekse1.wetherapp7.R
+import com.proalekse1.wetherapp7.adapters.WeatherAdapter
+import com.proalekse1.wetherapp7.adapters.WeatherModel
+import com.proalekse1.wetherapp7.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
-
+    private lateinit var binding: FragmentHoursBinding //подключили байндинг
+    private lateinit var adapter: WeatherAdapter //инициализировали адаптер
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)//подключили байндинг
+        return binding.root//подключили байндинг
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { //создается фрагмент
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){ //инициализируем адаптер
+        rcView.layoutManager = LinearLayoutManager(activity) //указываем как хотим чтобы ресайклер вью показывался
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf( //временный список чтобы передать в submitList(), пока ничего с сервера не получаем
+            WeatherModel("", "12:00",
+                "Sunny", "25°C",
+                "", "", "", ""),
+            WeatherModel("", "13:00",
+                "Sunny", "27°C",
+                "", "", "", ""),
+            WeatherModel("", "14:00",
+                "Sunny", "35°C",
+                "", "", "", "")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
