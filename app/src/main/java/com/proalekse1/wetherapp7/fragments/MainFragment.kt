223b8556b2bfc5653e53b0753c13cb423bdcd6c6
proalekse1 <proaleekse1@yandex.ru>
2022-12-01 08:57:17 +0300

    add API to MainFragment

--- app/src/main/java/com/proalekse1/wetherapp7/fragments/MainFragment.kt
+++ app/src/main/java/com/proalekse1/wetherapp7/fragments/MainFragment.kt
@@ -3,6 +3,7 @@ package com.proalekse1.wetherapp7.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +12,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayoutMediator
 import com.proalekse1.wetherapp7.adapters.VpAdapter
 import com.proalekse1.wetherapp7.databinding.FragmentMainBinding
 import com.proalekse1.wetherapp7.isPermissionGranted
-
+const val API_KEY = "ee30c89e35434b60b29163436223011" //константа для api ключа
 
 class MainFragment : Fragment() {
     private lateinit var pLauncher: ActivityResultLauncher<String> //для диалога и разрешений
@@ -41,6 +45,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission() // диалог разрешений
         init() //VpAdapter for ViewPager на разметке
+        requestWeatherData("Tula")
     }
 
     private fun init() = with(binding){ //VpAdapter for ViewPager на разметке
@@ -64,6 +69,28 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String){ //функция отправки и получения результата
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q="+
+                city+
+                "&days="+
+                "3"+
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context) //создаем очередь для запроса
+        val request = StringRequest( //хотим получить поэтому get //создаем запрос
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("MyLog", "Result: $result") //слушатель(в виде лямбды) который будет ждать результат который получим
+            },
+            {
+               error -> Log.d("MyLog", "Error: $error") //слушатель(в виде лямбды) который будет ждать результат который получим
+            }
+        )
+        queue.add(request) //добавляем в очередь запрос
     }
 
     companion object {
